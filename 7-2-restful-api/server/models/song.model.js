e3ac9bd87a171fd278bfa0a85303fe3732290813
Layan-2004 <layanalasmari25@gmail.com>
2026-04-15 13:02:22 +0300

    task2

--- 7-2-restful-api/server/models/song.model.js
+++ 7-2-restful-api/server/models/song.model.js
@@ -1,5 +1,11 @@
 import mongoose from "mongoose";
 
 // db schema
+const songSchema = new mongoose.Schema({
+        title:  { type: String, required: true, trim: true },
+        artist: { type: String, required: true, trim: true },
+        year:   { type: Number, min: 0, max: 2030 }
+      }, { timestamps: true });
+
 
 export const Song = mongoose.model("Song", songSchema);
